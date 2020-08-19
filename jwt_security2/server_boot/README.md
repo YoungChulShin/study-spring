aa7ec8c1f93a9f05b438e71f75958af5c17900da
YoungChulShin <go1323@gmail.com>
2020-08-19 10:50:05 +0900

    README 업데이트

---
+++ jwt_security2/server_boot/README.md
@@ -0,0 +1,5 @@
+### 저장소 설명
+Spring Security + jwt 실습 
+
+### 참고 사이트
+https://sun-22.tistory.com/46?category=364993
---
+++ jwt_security2/server_boot/src/main/java/study/spring/auth/bootserver/BootserverApplication.java
@@ -0,0 +1,13 @@
+package study.spring.auth.bootserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootserverApplication.class, args);
+    }
+
+}
