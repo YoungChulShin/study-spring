35e356b8b58586370e747afb9f722f0dece037e7
YoungChulShin <go1323@gmail.com>
2020-09-08 21:07:06 +0900

    test controller 추가

--- inflearn_querydsl/src/main/java/study/spring/querydsl/controller/InitMember.java
+++ inflearn_querydsl/src/main/java/study/spring/querydsl/controller/InitMember.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import study.spring.querydsl.entity.Member;
+import study.spring.querydsl.entity.Team;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.temporal.TemporalAmount;
@@ -14,6 +17,14 @@ import java.time.temporal.TemporalAmount;
 @RequiredArgsConstructor
 public class InitMember {
 
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    @Component
     static class InitMemberService {
 
         @PersistenceContext
@@ -21,7 +32,15 @@ public class InitMember {
 
         @Transactional
         public void init() {
-            Team team =
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+            em.persist(teamA);
+            em.persist(teamB);
+
+            for (int i = 0; i < 100; i++) {
+                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member" + i, i, selectedTeam));
+            }
         }
     }
 }
