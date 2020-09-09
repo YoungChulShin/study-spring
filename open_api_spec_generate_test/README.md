6d131816d2c01bd5c00b6db9d57765cf2a2295df
YoungChulShin <go1323@gmail.com>
2020-09-09 17:51:50 +0900

    open api spec 초기 소스

---
+++ open_api_spec_generate_test/README.md
@@ -0,0 +1,23 @@
+### 목적
+Open Api Spec. 을 이용해서 코드 생성 및 활용을 테스트 한다
+
+### API Spec. 
+간단한 Member 을 생성하고, 수정하고, 조회하고, 삭제하는 Api를 만들어본다
+
+Member 생성
+- POST
+- /api/members
+- name, age, gender
+
+Member 수정
+- Put
+- /api/members/{member id}
+- name, age, gender
+
+Member 삭제
+- Delete
+- /api/members/{member id}
+
+Member 조회
+- Get
+- /api/members, /api/members/{member id}
---
+++ open_api_spec_generate_test/src/main/java/study/java/openapispecgeneratetest/OpenApiSpecGenerateTestApplication.java
@@ -0,0 +1,13 @@
+package study.java.openapispecgeneratetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiSpecGenerateTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiSpecGenerateTestApplication.class, args);
+    }
+
+}
