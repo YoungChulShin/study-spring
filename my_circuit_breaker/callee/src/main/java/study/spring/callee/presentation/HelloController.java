76c4c953d80c9a7a3dfe90ddded5f81db349173e
YoungChulShin <go1323@gmail.com>
2022-02-05 22:59:07 +0900

    서킷브레이커 callee 추가

---
+++ my_circuit_breaker/callee/src/main/java/study/spring/callee/presentation/HelloController.java
@@ -0,0 +1,14 @@
+package study.spring.callee.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/greeting/hello/{name}")
+  public String hello(@PathVariable(value = "name") String name) {
+    return "hello, " + name;
+  }
+}
