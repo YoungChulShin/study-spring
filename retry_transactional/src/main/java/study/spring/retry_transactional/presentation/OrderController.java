f22d82a76851d762394192b95411d710a121ebe6
YoungChulShin <go1323@gmail.com>
2022-02-16 19:09:12 +0900

    Controller 추가

---
+++ retry_transactional/src/main/java/study/spring/retry_transactional/presentation/OrderController.java
@@ -0,0 +1,28 @@
+package study.spring.retry_transactional.presentation;
+
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import study.spring.retry_transactional.application.OrderFacade;
+import study.spring.retry_transactional.domain.OrderInfo;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderController {
+
+  private final OrderFacade orderFacade;
+
+  @GetMapping("/orders/{orderId}")
+  public OrderInfo getOrder(@PathVariable(name = "orderId") Long orderId) {
+    return orderFacade.getOrder(orderId);
+  }
+
+  @PostMapping("/orders")
+  public Long registerOrder(@Valid @RequestBody RegisterOrderRequest request) {
+    return orderFacade.registerOrder(request.getOrderNumber());
+  }
+}
