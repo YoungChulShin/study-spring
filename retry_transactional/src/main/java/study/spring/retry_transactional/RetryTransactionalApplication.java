afab1171361058e45cd70cd7075cf977e7674480
YoungChulShin <go1323@gmail.com>
2022-02-16 17:39:12 +0900

    소스 초안

---
+++ retry_transactional/src/main/java/study/spring/retry_transactional/RetryTransactionalApplication.java
@@ -0,0 +1,13 @@
+package study.spring.retry_transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryTransactionalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RetryTransactionalApplication.class, args);
+  }
+
+}
---
+++ retry_transactional/src/test/java/study/spring/retry_transactional/RetryTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.retry_transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetryTransactionalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
