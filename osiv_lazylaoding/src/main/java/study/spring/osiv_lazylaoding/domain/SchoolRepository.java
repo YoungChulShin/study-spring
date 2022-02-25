5ec46615c151fbf4adaa5ae5be306c89029ecbb7
YoungChulShin <go1323@gmail.com>
2022-02-25 22:03:03 +0900

    repository 추가

---
+++ osiv_lazylaoding/src/main/java/study/spring/osiv_lazylaoding/domain/SchoolRepository.java
@@ -0,0 +1,7 @@
+package study.spring.osiv_lazylaoding.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolRepository extends JpaRepository<School, Long> {
+
+}
---
+++ osiv_lazylaoding/src/main/java/study/spring/osiv_lazylaoding/domain/StudentRepository.java
@@ -0,0 +1,7 @@
+package study.spring.osiv_lazylaoding.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
