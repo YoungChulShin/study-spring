6789148342b6c831823e040e3e12927633cdc5b0
YoungChulShin <go1323@gmail.com>
2022-03-15 23:34:15 +0900

    redis ttl 만료 이벤트 리스너 추가

--- redis_cache2/src/main/java/study/spring/food_delivery/common/RedisConfig.java
+++ redis_cache2/src/main/java/study/spring/food_delivery/common/RedisConfig.java
@@ -3,6 +3,7 @@ package study.spring.food_delivery.common;
 import java.time.Duration;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +14,18 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import study.spring.food_delivery.domain.listener.AgentLocationExpirationListener;
 
+@Slf4j
 @EnableCaching
 @Configuration
 public class RedisConfig {
+
+  private final String EXPIRE_PATTERN = "__keyevent@*__:expired";
 
   @Bean
   public LettuceConnectionFactory redisConnectionFactory() {
@@ -56,4 +63,16 @@ public class RedisConfig {
         .withInitialCacheConfigurations(cacheConfigurations)
         .build();
   }
+
+  @Bean
+  public RedisMessageListenerContainer redisMessageListenerContainer(
+      RedisConnectionFactory connectionFactory,
+      AgentLocationExpirationListener agentLocationExpirationListener) {
+    RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+    container.setConnectionFactory(connectionFactory);
+    container.addMessageListener(agentLocationExpirationListener, new PatternTopic(EXPIRE_PATTERN));
+    container.setErrorHandler(e -> log.error(e.getMessage()));
+    return container;
+  }
+
 }
