2d201b0f190600070210866939c012083de26936
YoungChulShin <go1323@gmail.com>
2022-05-07 08:10:51 +0900

    테스트 코드 동작을  위한 코드 수정

--- testcontainers/junit5/src/main/java/study/spring/spring_test/domain/model/student/Student.java
+++ testcontainers/junit5/src/main/java/study/spring/spring_test/domain/model/student/Student.java
@@ -4,11 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Student {
 
   @Id
