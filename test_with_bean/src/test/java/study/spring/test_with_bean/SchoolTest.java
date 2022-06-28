c2f368c1b05143f1f8b0d661e82246816340d1a4
YoungChulShin <go1323@gmail.com>
2022-06-29 07:09:22 +0900

    application.properties를 이용해서 속성 값 입력

--- test_with_bean/src/test/java/study/spring/test_with_bean/SchoolTest.java
+++ test_with_bean/src/test/java/study/spring/test_with_bean/SchoolTest.java
@@ -12,11 +12,14 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {SchoolConfiguration.class})
-@TestPropertySource(properties = {
-    "application.school.name=ycschool",
-    "application.school.location=seoul gandong",
-    "application.school.age=30"
-})
+// 1. properties 속성을 이용해서 의존을 바로 주입해줄 수 있다
+//@TestPropertySource(properties = {
+//    "application.school.name=ycschool",
+//    "application.school.location=seoul gandong",
+//    "application.school.age=30"
+//})
+// 2. location을 지정할 수 있는데, 이 경우 *.properties 또는 *.xml 파일이어야한다
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class SchoolTest {
 
   @Autowired
@@ -24,6 +27,6 @@ public class SchoolTest {
 
   @Test
   void test() {
-    Assertions.assertThat(school.getName()).isEqualTo("ycschool");
+    Assertions.assertThat(school.getName()).isEqualTo("mjschool");
   }
 }
