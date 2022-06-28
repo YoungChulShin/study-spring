0be06462c01e21bcf46d68ff87bce46896835146
YoungChulShin <go1323@gmail.com>
2022-06-28 19:37:27 +0900

    프로젝트 초안

---
+++ test_with_bean/src/main/java/study/spring/test_with_bean/TestWithBeanApplication.java
@@ -0,0 +1,13 @@
+package study.spring.test_with_bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWithBeanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestWithBeanApplication.class, args);
+  }
+
+}
---
+++ test_with_bean/src/test/java/study/spring/test_with_bean/TestWithBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.test_with_bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWithBeanApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
