d8d60481b01051d48b411834759796f47a851550
YoungChulShin <go1323@gmail.com>
2022-06-30 23:06:05 +0900

    deserialize_unknownField 테스트

--- object_mapper_test/src/test/java/study/spring/object_mapper_test/MapperTest.java
+++ object_mapper_test/src/test/java/study/spring/object_mapper_test/MapperTest.java
@@ -1,14 +1,18 @@
 package study.spring.object_mapper_test;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MapperTest {
 
   @Test
-  void serializeAndDeserialize() throws JsonProcessingException {
+  void serialize_and_deserialize() throws JsonProcessingException {
     // given
     School school = new School("ycshin", "gangdong", 30);
 
@@ -20,5 +24,33 @@ public class MapperTest {
 
     // then
     Assertions.assertThat(deserializedSchool).isEqualTo(school);
+  }
+
+  @Test
+  void deserialize_unknownField_shouldThrowException() {
+    // given
+    ObjectMapper objectMapper = new ObjectMapper();
+    String valueAsString = "{\"name\":\"ycshin\",\"location\":\"gangdong\",\"age\":30,\"type\":\"highschool\"}";
+
+    // when
+    Throwable thrown = catchThrowable(() -> objectMapper.readValue(valueAsString, School.class));
+
+    // then
+    Assertions.assertThat(thrown).isNotNull();
+    Assertions.assertThat(thrown.getClass()).isEqualTo(UnrecognizedPropertyException.class);
+  }
+
+  @Test
+  void deserialize_unknownField_withOptions_FAIL_ON_UNKNOWN_PROPERTIES_shouldNotThrowException() {
+    // given
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.disable(FAIL_ON_UNKNOWN_PROPERTIES);
+    String valueAsString = "{\"name\":\"ycshin\",\"location\":\"gangdong\",\"age\":30,\"type\":\"highschool\"}";
+
+    // when
+    Throwable thrown = catchThrowable(() -> objectMapper.readValue(valueAsString, School.class));
+
+    // then
+    Assertions.assertThat(thrown).isNull();
   }
 }
