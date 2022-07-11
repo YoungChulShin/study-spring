52500d95626ea259e124b7276dc1c8ceec1ed380
YoungChulShin <go1323@gmail.com>
2022-07-12 07:24:15 +0900

    프로젝트 초안

---
+++ jpa_join_distinct_test/src/main/java/study/spring/jpa_join_distinct_test/JpaJoinDistinctTestApplication.java
@@ -0,0 +1,13 @@
+package study.spring.jpa_join_distinct_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaJoinDistinctTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpaJoinDistinctTestApplication.class, args);
+  }
+
+}
---
+++ jpa_join_distinct_test/src/test/java/study/spring/jpa_join_distinct_test/JpaJoinDistinctTestApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.jpa_join_distinct_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaJoinDistinctTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
