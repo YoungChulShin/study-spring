5250f1510a01713c12e11efbcb1c6d66b24fbcc7
YoungChulShin <go1323@gmail.com>
2022-07-15 19:14:18 +0900

    프로젝트 초안

---
+++ message_rabbitmq/README.md
@@ -0,0 +1,3 @@
+# RabbitMQ 메시지 테스트
+
+https://spring.io/guides/gs/messaging-rabbitmq/ 참고해서 테스트
---
+++ message_rabbitmq/src/main/java/study/spring/message_rabbitmq/MessageRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package study.spring.message_rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageRabbitmqApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MessageRabbitmqApplication.class, args);
+  }
+
+}
