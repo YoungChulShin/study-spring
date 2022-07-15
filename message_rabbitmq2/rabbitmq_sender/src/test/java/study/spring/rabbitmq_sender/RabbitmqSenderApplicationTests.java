754ff7c45166544dec7ce3727ed732f3f11bcbf9
YoungChulShin <go1323@gmail.com>
2022-07-16 08:13:28 +0900

    프로젝트 초안

---
+++ message_rabbitmq2/rabbitmq_sender/src/test/java/study/spring/rabbitmq_sender/RabbitmqSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.rabbitmq_sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSenderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
