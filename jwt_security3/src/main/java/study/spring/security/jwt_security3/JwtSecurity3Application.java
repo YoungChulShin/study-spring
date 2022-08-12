9d029ecbd431d35a2eb85889309b108b6f7219ad
YoungChulShin <go1323@gmail.com>
2022-08-13 07:36:47 +0900

    프로젝트 스캐폴드

---
+++ jwt_security3/src/main/java/study/spring/security/jwt_security3/JwtSecurity3Application.java
@@ -0,0 +1,13 @@
+package study.spring.security.jwt_security3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurity3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JwtSecurity3Application.class, args);
+  }
+
+}
---
+++ jwt_security3/src/test/java/study/spring/security/jwt_security3/JwtSecurity3ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.security.jwt_security3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurity3ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
