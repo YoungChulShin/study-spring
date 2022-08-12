a9956bf7a77e9923e604df5c28ba99e952f708ae
YoungChulShin <go1323@gmail.com>
2022-08-13 08:39:12 +0900

    서비스 로그 추가

--- jwt_security3/src/main/java/study/spring/security/jwt_security3/service/UserServiceImpl.java
+++ jwt_security3/src/main/java/study/spring/security/jwt_security3/service/UserServiceImpl.java
@@ -21,16 +21,19 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public User saveUser(User user) {
+    log.info("Saving new user {} to database", user.getName());
     return userRepo.save(user);
   }
 
   @Override
   public Role saveRole(Role role) {
+    log.info("Saving new role {} to database", role.getName());
     return roleRepo.save(role);
   }
 
   @Override
   public void addRoleToUser(String username, String roleName) {
+    log.info("Adding role {} to user {}", roleName, username);
     User user = userRepo.findByUsername(username);
     Role role = roleRepo.findbyName(roleName);
 
@@ -39,11 +42,13 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public User getUser(String username) {
+    log.info("Fetching user {}", username);
     return userRepo.findByUsername(username);
   }
 
   @Override
   public List<User> getUsers() {
+    log.info("Fetching all users");
     return userRepo.findAll();
   }
 }
