a5f5303ee9118284d88d5507f17bfae9a229bb42
YoungChulShin <go1323@gmail.com>
2022-08-13 22:54:42 +0900

    컨트롤러 추가

--- jwt_security3/src/main/java/study/spring/security/jwt_security3/api/UserResource.java
+++ jwt_security3/src/main/java/study/spring/security/jwt_security3/api/UserResource.java
@@ -1,11 +1,17 @@
 package study.spring.security.jwt_security3.api;
 
+import java.net.URI;
 import java.util.List;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import study.spring.security.jwt_security3.domain.Role;
 import study.spring.security.jwt_security3.domain.User;
 import study.spring.security.jwt_security3.service.UserService;
 
@@ -19,5 +25,29 @@ public class UserResource {
   @GetMapping("/users")
   public ResponseEntity<List<User>> getUsers() {
     return ResponseEntity.ok().body(userService.getUsers());
+  }
+
+  @PostMapping("/user/save")
+  public ResponseEntity<User> saveUser(@RequestBody User user) {
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+    return ResponseEntity.created(uri).body(userService.saveUser(user));
+  }
+
+  @PostMapping("/role/save")
+  public ResponseEntity<Role> saveUser(@RequestBody Role role) {
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+    return ResponseEntity.created(uri).body(userService.saveRole(role));
+  }
+
+  @PostMapping("/role/addtouser")
+  public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
+    userService.addRoleToUser(form.username, form.roleName);
+    return ResponseEntity.ok().build();
+  }
+
+  @Data
+  class RoleToUserForm {
+    private String username;
+    private String roleName;
   }
 }
