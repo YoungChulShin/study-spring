61127c634e16f2b47f7dad55928d0b7a85654ebe
YoungChulShin <go1323@gmail.com>
2022-08-14 12:06:07 +0900

    login 시 jwt 토큰 응답 추가

--- jwt_security3/src/main/java/study/spring/security/jwt_security3/filter/CustomAuthenticationFilter.java
+++ jwt_security3/src/main/java/study/spring/security/jwt_security3/filter/CustomAuthenticationFilter.java
@@ -1,15 +1,26 @@
 package study.spring.security.jwt_security3.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
@@ -17,10 +28,14 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
   private final AuthenticationManager authenticationManager;
 
+  // AuthenticationManager는
+  // UserDetailsService를 사용
+  // loadUserByUsername를 이용해서 사용자 정보를 가져온다
   public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
     this.authenticationManager = authenticationManager;
   }
 
+  // Access를 시도할 때 기능 오버라이드
   @Override
   public Authentication attemptAuthentication(
       HttpServletRequest request,
@@ -34,9 +49,35 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     return authenticationManager.authenticate(authenticationToken);
   }
 
+  // 인증 성공했을 때 기능 오버라이드
   @Override
-  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
-      FilterChain chain, Authentication authResult) throws IOException, ServletException {
-    super.successfulAuthentication(request, response, chain, authResult);
+  protected void successfulAuthentication(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain chain,
+      Authentication authentication) throws IOException, ServletException {
+    User user = (User)authentication.getPrincipal();
+    Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+    String accessToken = JWT.create()
+        .withSubject(user.getUsername())
+        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+        .withIssuer(request.getRequestURL().toString())
+        .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+        .sign(algorithm);
+    String refreshToken = JWT.create()
+        .withSubject(user.getUsername())
+        .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+        .withIssuer(request.getRequestURL().toString())
+        .sign(algorithm);
+
+    response.setHeader("access_token", accessToken);
+    response.setHeader("refresh_token", refreshToken);
+
+    Map<String, String> tokens = new HashMap<>();
+    tokens.put("access_token", accessToken);
+    tokens.put("refresh_token", refreshToken);
+
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    new ObjectMapper().writeValue(response.getOutputStream(), tokens);
   }
 }
