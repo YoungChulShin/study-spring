4f4d984f19c1b38efebb02e16e04c832b815b0c4
YoungChulShin <go1323@gmail.com>
2022-09-10 14:44:53 +0900

    프로젝트 스캐폴드

---
+++ webflux-test/src/test/java/study/spring/webflux/webfluxtest/WebfluxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.webflux.webfluxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
