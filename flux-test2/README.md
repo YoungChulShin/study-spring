eb17a169b1a1ccd1ed138b87cf7857d73e53abd7
YoungChulShin <go1323@gmail.com>
2022-09-10 21:59:32 +0900

    프로젝트 스캐폴드

---
+++ flux-test2/README.md
@@ -0,0 +1,8 @@
+# 저장소 설명
+스프링 웹플럭스 테스트 샘플 코딩
+- https://youtu.be/rQXAvCNsZ8I?list=PL93mKxaRDidFH5gRwkDX5pQxtp0iv3guf
+
+사용 기술
+- webflux
+- h2
+- r2dbc
---
+++ flux-test2/src/main/java/study/spring/webflux/fluxtest2/FluxTest2Application.java
@@ -0,0 +1,13 @@
+package study.spring.webflux.fluxtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxTest2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FluxTest2Application.class, args);
+  }
+
+}
