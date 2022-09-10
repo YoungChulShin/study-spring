22529ee7737afbbdb221c4fcd63d673f7110387a
YoungChulShin <go1323@gmail.com>
2022-09-11 07:08:25 +0900

    sse 방식

--- flux-test2/src/main/java/study/spring/webflux/fluxtest2/web/CustomerController.java
+++ flux-test2/src/main/java/study/spring/webflux/fluxtest2/web/CustomerController.java
@@ -8,14 +8,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 import study.spring.webflux.fluxtest2.domain.Customer;
 import study.spring.webflux.fluxtest2.domain.CustomerRepository;
 
 @RestController
-@RequiredArgsConstructor
 public class CustomerController {
 
   private final CustomerRepository customerRepository;
+  private final Sinks.Many<Customer> sink;
+
+  public CustomerController(CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
+    this.sink = Sinks.many().multicast().onBackpressureBuffer();
+  }
 
   /**
    * 호출 순서
@@ -34,6 +40,13 @@ public class CustomerController {
     return customerRepository.findAll().log();
   }
 
+  @GetMapping(value = "/customer-stream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+  public Flux<Customer> findAllWithStream() {
+    return customerRepository.findAll().delayElements(Duration.ofSeconds(1)).log();
+  }
+
+
+
   @GetMapping("/customer/{id}")
   public Mono<Customer> findById(@PathVariable Long id) {
     return customerRepository.findById(id).log();
@@ -49,5 +62,12 @@ public class CustomerController {
   @GetMapping(value = "/fluxstream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
   public Flux<Integer> fluxstream() {
     return Flux.just(1, 2, 3, 4, 5).delayElements(Duration.ofSeconds(1)).log();
+  }
+
+  // sse: server-sent-devent
+  // text/event-stream 이면 가능하다
+  @GetMapping(value = "/customer/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<Customer> findAllSSE() {
+    return customerRepository.findAll().delayElements(Duration.ofSeconds(1)).log();
   }
 }
