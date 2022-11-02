6fbad810d7dcd0b208f84fdfb40cd233a5c22164
YoungChulShin <go1323@gmail.com>
2022-11-03 05:32:16 +0900

    README 업데이트

---
+++ webflux-test3/README.md
@@ -0,0 +1,3 @@
+# 저장소 설명
+스프링 웹플럭스 테스트
+- https://youtu.be/PecY7og5KyI
---
+++ webflux-test3/src/test/java/study/spring/webfluxtest3/WebfluxTest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.webfluxtest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxTest3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
