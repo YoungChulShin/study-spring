31c6f100b240d2486088177ae6d300eff09a3ff0
YoungChulShin <go1323@gmail.com>
2022-11-03 21:10:25 +0900

    README 추가

---
+++ executor-queue-handling-test/src/main/java/study/java/executorqueuehandlingtest/ExecutorQueueHandlingTestApplication.java
@@ -0,0 +1,13 @@
+package study.java.executorqueuehandlingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutorQueueHandlingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExecutorQueueHandlingTestApplication.class, args);
+    }
+
+}
---
+++ executor-queue-handling-test/src/test/java/study/java/executorqueuehandlingtest/ExecutorQueueHandlingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package study.java.executorqueuehandlingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutorQueueHandlingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
