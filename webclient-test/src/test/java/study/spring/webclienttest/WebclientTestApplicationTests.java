58b75cc25140a5cfb58bafb1792733054e1ad82b
YoungChulShin <go1323@gmail.com>
2022-12-12 19:30:09 +0900

    프로젝트 스캐폴드

---
+++ webclient-test/src/test/java/study/spring/webclienttest/WebclientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.webclienttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
