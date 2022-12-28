5eeca206c8c2bf86f3bcd3afb8ee38cb466439f7
YoungChulShin <go1323@gmail.com>
2022-12-28 22:41:34 +0900

    r2dbc custom repository 추가 및 mysql connector 변경

--- webflux-performance-test/webflux-sample/src/main/java/study/webflux/webfluxsample/application/StudentService.java
+++ webflux-performance-test/webflux-sample/src/main/java/study/webflux/webfluxsample/application/StudentService.java
@@ -1,13 +1,10 @@
 package study.webflux.webfluxsample.application;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuples;
 import study.webflux.webfluxsample.domain.*;
-
-import java.util.List;
 
 @Service
 public class StudentService {
@@ -20,21 +17,18 @@ public class StudentService {
         this.schoolRepository = schoolRepository;
     }
 
-    @Transactional
     public Mono<StudentInfo> createStudent(CreateStudentCommand command) {
         return schoolRepository.findByName(command.schoolName())
                 .switchIfEmpty(Mono.error(new RuntimeException("학교 정보를 찾을 수 없습니다")))
-                .map(school -> Tuples.of(school, new Student(
+                .map(school -> new Student(
                         command.name(),
                         command.age(),
                         command.gender(),
-                        school.getId()
-                )))
-                .flatMap(t -> studentRepository.save(t.getT2()))
-                .map(t -> StudentInfo.from(t, new School("test")));
+                        school.getId()))
+                .flatMap(studentRepository::save)
+                .map(student -> StudentInfo.from(student, new School("test")));
     }
 
-    @Transactional(readOnly = true)
     public Flux<StudentInfo> findStudents() {
         return studentRepository.findAllWithSchool();
     }
--- webflux-performance-test/webflux-sample/src/main/java/study/webflux/webfluxsample/domain/StudentRepository.java
+++ webflux-performance-test/webflux-sample/src/main/java/study/webflux/webfluxsample/domain/StudentRepository.java
@@ -1,16 +1,14 @@
 package study.webflux.webfluxsample.domain;
 
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
 
-public interface StudentRepository extends ReactiveCrudRepository<Student, Long> {
+public interface StudentRepository extends ReactiveCrudRepository<Student, Long>, StudentCustomRepository {
 
-    @Query("""
-        SELECT s.id, s.name, s.age, s.gender, sc.name as schoolName
-        FROM  students s
-        INNER JOIN  schools sc
-                ON  s.school_id = sc.id
-""")
-    Flux<StudentInfo> findAllWithSchool();
+//    @Query("""
+//        SELECT s.id, s.name, s.age, s.gender, sc.name as schoolName
+//        FROM  students s
+//        INNER JOIN  schools sc
+//                ON  s.school_id = sc.id
+//""")
+//    Flux<StudentInfo> findAllWithSchool();
 }
