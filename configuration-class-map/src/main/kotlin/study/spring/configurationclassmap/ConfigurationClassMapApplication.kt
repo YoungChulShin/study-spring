f8eea4e834dfba39c4c24f67c3430e61b68ce257
YoungChulShin <go1323@gmail.com>
2023-01-07 06:00:09 +0900

    프로젝트 초안

---
+++ configuration-class-map/src/main/kotlin/study/spring/configurationclassmap/ConfigurationClassMapApplication.kt
@@ -0,0 +1,11 @@
+package study.spring.configurationclassmap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConfigurationClassMapApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConfigurationClassMapApplication>(*args)
+}
---
+++ configuration-class-map/src/test/kotlin/study/spring/configurationclassmap/ConfigurationClassMapApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.spring.configurationclassmap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigurationClassMapApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
