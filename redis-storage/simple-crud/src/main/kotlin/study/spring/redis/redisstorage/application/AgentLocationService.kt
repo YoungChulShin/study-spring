9a2b227c58a8dc5dd5f91d9ecdebaeb3b229934d
YoungChulShin <go1323@gmail.com>
2023-01-07 23:32:29 +0900

    hash 데이터 저장

--- redis-storage/simple-crud/src/main/kotlin/study/spring/redis/redisstorage/application/AgentLocationService.kt
+++ redis-storage/simple-crud/src/main/kotlin/study/spring/redis/redisstorage/application/AgentLocationService.kt
@@ -1,12 +1,9 @@
 package study.spring.redis.redisstorage.application
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 import study.spring.redis.redisstorage.domain.AgentLocation
-import java.lang.RuntimeException
-import java.time.temporal.ChronoUnit
 import java.util.concurrent.TimeUnit
 
 @Service
@@ -14,7 +11,12 @@ class AgentLocationService(
     private val redisTemplate: StringRedisTemplate,
     private val objectMapper: ObjectMapper,
 ) {
+    companion object {
+        private const val KEY_PREFIX_AGENT_LOCATION_HASH = "agent:"
+    }
+
     fun saveLocation(agentId: Long, request: AgentLocation): AgentLocation {
+        // Strings 데이터 저장
         val ops = redisTemplate.opsForValue()
         ops.set(
             agentId.toString(),
@@ -23,13 +25,27 @@ class AgentLocationService(
             TimeUnit.SECONDS
         )
 
+        // Hash 데이터 저장
+        val opsForHash = redisTemplate.opsForHash<String, String>()
+        val map = mutableMapOf(
+            "longitude" to request.longitude.toString(),
+            "latitude" to request.latitude.toString(),
+        )
+        opsForHash.putAll(KEY_PREFIX_AGENT_LOCATION_HASH + agentId, map)
+
         return request
     }
 
     fun getLocation(agentId: Long): AgentLocation {
-        val value = redisTemplate.opsForValue().get(agentId.toString())
-            ?: throw RuntimeException("위치 정보가 없습니다")
-
-        return objectMapper.readValue(value, AgentLocation::class.java)
+        return redisTemplate.opsForValue().get(agentId.toString())
+            ?.let { objectMapper.readValue(it, AgentLocation::class.java) }
+            ?: redisTemplate.opsForHash<String, String>().entries(KEY_PREFIX_AGENT_LOCATION_HASH + agentId)
+                .let {
+                    if (it.size < 2) throw java.lang.RuntimeException("위치 데이터가 없습니다")
+                    AgentLocation(
+                        it["latitude"]!!.toDouble(),
+                        it["longitude"]!!.toDouble()
+                    )
+                }
     }
 }
