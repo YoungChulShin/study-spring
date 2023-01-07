f159a05733788d5014644a919ec9abcef8338372
YoungChulShin <go1323@gmail.com>
2023-01-07 17:07:33 +0900

    redisConnectionFactory 추가

---
+++ redis-storage/simple-crud/src/main/kotlin/study/spring/redis/redisstorage/config/RedisConfiguration.kt
@@ -0,0 +1,21 @@
+package study.spring.redis.redisstorage.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
+@Configuration
+class RedisConfiguration(
+    @Value("\${spring.redis.host}") private val host: String,
+    @Value("\${spring.redis.port}") private val port: Int,
+) {
+
+    @Bean
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory(
+            RedisStandaloneConfiguration(this.host, this.port)
+        )
+    }
+}
