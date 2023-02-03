bad38317519c04192dd9c8a89083e379f897697e
YoungChulShin <go1323@gmail.com>
2023-02-04 02:01:14 +0900

    방법1. Conditional

--- bean-conditional/README.md
+++ bean-conditional/README.md
@@ -1,8 +1,9 @@
-package study.spring.beanconditional
+# Bean Conditional
+빈이 충돌할 때 어떻게 이를 피해서 사용할 수 있는 지를 알아본다
 
-import org.springframework.context.annotation.*
-import org.springframework.core.type.AnnotatedTypeMetadata
-
+### Conditional
+`Conditional`애노테이션을 이용해서 빈의 등록 여부를 설정한다
+```java
 @Configuration
 @Conditional(TruePrinterCondition::class)
 class TruePrinterConfiguration {
@@ -17,3 +18,5 @@ class TruePrinterCondition: Condition {
     }
 
 }
+```
+
--- bean-conditional/src/main/kotlin/study/spring/beanconditional/FalsePrinterConfiguration.kt
+++ bean-conditional/src/main/kotlin/study/spring/beanconditional/FalsePrinterConfiguration.kt
@@ -4,8 +4,16 @@ import org.springframework.context.annotation.*
 import org.springframework.core.type.AnnotatedTypeMetadata
 
 @Configuration
+@Conditional(FalsePrinterCondition::class)
 class FalsePrinterConfiguration {
 
     @Bean
     fun falsePrinter(): BooleanPrinter = FalsePrinter()
+}
+
+class FalsePrinterCondition: Condition {
+    override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
+        return false
+    }
+
 }
