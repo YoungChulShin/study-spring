6517802e432c7e0a2255e6782f65abc7a65ce3b0
YoungChulShin <go1323@gmail.com>
2023-02-18 07:43:03 +0900

    caching annotation을 이용한 multievict 처리

--- redis-cache/src/main/kotlin/study/spring/redis/rediscache/config/RedisConfig.kt
+++ redis-cache/src/main/kotlin/study/spring/redis/rediscache/config/RedisConfig.kt
@@ -1,5 +1,7 @@
 package study.spring.redis.rediscache.config
 
+import com.fasterxml.jackson.databind.JavaType
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
@@ -7,12 +9,16 @@ import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import java.time.Duration
+import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 @Configuration
-class RedisConfig {
+class RedisConfig(
+    private val objectMapper: ObjectMapper,
+) {
 
     @Bean
     fun redisConnectionFactory(): LettuceConnectionFactory {
@@ -24,8 +30,16 @@ class RedisConfig {
         val configuration = getDefaultConfiguration()
 
         val keyConfiguration = mutableMapOf(
-            CacheKey.FIND_USERS to getDefaultConfiguration().entryTtl(Duration.ofSeconds(CacheKey.FIND_USERS_TTL)),
-            CacheKey.FIND_USER to getDefaultConfiguration().entryTtl(Duration.ofSeconds(CacheKey.FIND_USER_TTL)),
+            CacheKey.FIND_USERS to getDefaultConfiguration()
+                .entryTtl(Duration.ofSeconds(CacheKey.FIND_USERS_TTL))
+                .serializeValuesWith(
+                    RedisSerializationContext.SerializationPair.fromSerializer(
+                        Jackson2JsonRedisSerializer(objectMapper, Set::class.java)
+                    )
+                )
+            ,
+            CacheKey.FIND_USER to getDefaultConfiguration()
+                .entryTtl(Duration.ofSeconds(CacheKey.FIND_USER_TTL)),
         )
 
         return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory)
@@ -37,14 +51,24 @@ class RedisConfig {
     private fun getDefaultConfiguration() =
         RedisCacheConfiguration.defaultCacheConfig()
             .disableCachingNullValues()
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    StringRedisSerializer()
+                )
+            )
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    Jackson2JsonRedisSerializer(objectMapper, Object::class.java)
+                )
+            )
             .entryTtl(Duration.ofSeconds(CacheKey.DEFAULT_TTL))
 }
 
 class CacheKey {
     companion object {
-        const val DEFAULT_TTL = 30L
+        const val DEFAULT_TTL = 120L
         const val FIND_USERS = "findUsers"
-        const val FIND_USERS_TTL = 60L
+        const val FIND_USERS_TTL = 120L
         const val FIND_USER = "findUser"
         const val FIND_USER_TTL = 120L
     }
--- redis-cache/src/main/kotlin/study/spring/redis/rediscache/service/UserService.kt
+++ redis-cache/src/main/kotlin/study/spring/redis/rediscache/service/UserService.kt
@@ -2,18 +2,22 @@ package study.spring.redis.rediscache.service
 
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.cache.annotation.Caching
+import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.stereotype.Service
 import study.spring.redis.rediscache.config.CacheKey
 
 @Service
-class UserService {
+class UserService(
+    private val cacheManager: RedisCacheManager,
+) {
 
     companion object {
         private val users = mutableSetOf<String>()
     }
 
     @Cacheable(
-        value = [CacheKey.FIND_USERS]
+        value = [CacheKey.FIND_USERS],
     )
     fun findUsers(): Set<String> {
         Thread.sleep(3000)
@@ -29,12 +33,26 @@ class UserService {
         return users.firstOrNull { it == name }
     }
 
-    @CacheEvict("test")
+    @CacheEvict(
+        value = [CacheKey.FIND_USERS],
+        allEntries = true,
+    )
     fun addUser(name: String) {
         users.add(name)
     }
 
-    @CacheEvict("test")
+    @Caching(
+        evict = [
+            CacheEvict(
+                value = [CacheKey.FIND_USERS],
+                allEntries = true,
+            ),
+            CacheEvict(
+                value = [CacheKey.FIND_USER],
+                key = "#name",
+            )
+        ]
+    )
     fun removeUser(name: String) {
         users.remove(name)
     }
