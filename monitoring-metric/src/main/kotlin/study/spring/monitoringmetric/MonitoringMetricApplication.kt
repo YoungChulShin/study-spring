6cf9551bbcb754b9cb1ad6cc22eb1a62be8d4a6c
YoungChulShin <go1323@gmail.com>
2023-03-19 22:27:55 +0900

    프로젝트 추가

---
+++ monitoring-metric/src/main/kotlin/study/spring/monitoringmetric/MonitoringMetricApplication.kt
@@ -0,0 +1,11 @@
+package study.spring.monitoringmetric
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonitoringMetricApplication
+
+fun main(args: Array<String>) {
+    runApplication<MonitoringMetricApplication>(*args)
+}
---
+++ monitoring-metric/src/test/kotlin/study/spring/monitoringmetric/MonitoringMetricApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.spring.monitoringmetric
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonitoringMetricApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
