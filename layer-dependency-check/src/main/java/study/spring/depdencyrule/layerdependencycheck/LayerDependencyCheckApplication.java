2c7b1a50e01765f5e6027dea6b9e7265f0c2b3e7
YoungChulShin <go1323@gmail.com>
2023-06-28 08:36:52 +0900

    프로젝트 추가

---
+++ layer-dependency-check/src/main/java/study/spring/depdencyrule/layerdependencycheck/LayerDependencyCheckApplication.java
@@ -0,0 +1,13 @@
+package study.spring.depdencyrule.layerdependencycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayerDependencyCheckApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LayerDependencyCheckApplication.class, args);
+  }
+
+}
---
+++ layer-dependency-check/src/test/java/study/spring/depdencyrule/layerdependencycheck/LayerDependencyCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.depdencyrule.layerdependencycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayerDependencyCheckApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
