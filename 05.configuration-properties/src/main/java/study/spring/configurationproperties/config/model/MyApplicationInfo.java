de289061a6e638d19094ecd46896797867a6fc3b
YoungChulShin <go1323@gmail.com>
2023-08-02 07:36:55 +0900

    04-configuration-properties: MyApplicationInfo 속성 추가

--- 05.configuration-properties/src/main/java/study/spring/configurationproperties/config/model/MyApplicationInfo.java
+++ 05.configuration-properties/src/main/java/study/spring/configurationproperties/config/model/MyApplicationInfo.java
@@ -1,11 +1,16 @@
 package study.spring.configurationproperties.config.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "my-application")
 public class MyApplicationInfo {
 
   private String name;
+  private String version;
+  private String releaseDate;
+  private List<Developer> developers = new ArrayList<>();
 
   public String getName() {
     return name;
@@ -13,5 +18,70 @@ public class MyApplicationInfo {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
+  public void setVersion(String version) {
+    this.version = version;
+  }
+
+  public String getReleaseDate() {
+    return releaseDate;
+  }
+
+  public void setReleaseDate(String releaseDate) {
+    this.releaseDate = releaseDate;
+  }
+
+  public List<Developer> getDevelopers() {
+    return developers;
+  }
+
+  public static class Developer {
+
+    private String name;
+    private String email;
+    private String company;
+    private String phone;
+    private List<String> skills = new ArrayList<>();
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public String getEmail() {
+      return email;
+    }
+
+    public void setEmail(String email) {
+      this.email = email;
+    }
+
+    public String getCompany() {
+      return company;
+    }
+
+    public void setCompany(String company) {
+      this.company = company;
+    }
+
+    public String getPhone() {
+      return phone;
+    }
+
+    public void setPhone(String phone) {
+      this.phone = phone;
+    }
+
+    public List<String> getSkills() {
+      return skills;
+    }
   }
 }
