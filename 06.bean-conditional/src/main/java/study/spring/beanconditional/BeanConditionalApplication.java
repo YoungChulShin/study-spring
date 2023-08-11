4c01328cd18d5e208f4d9ba3c2ae7990af3d4a00
YoungChulShin <go1323@gmail.com>
2023-08-11 23:22:54 +0900

    프로젝트 초안

---
+++ 06.bean-conditional/src/main/java/study/spring/beanconditional/BeanConditionalApplication.java
@@ -0,0 +1,13 @@
+package study.spring.beanconditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanConditionalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BeanConditionalApplication.class, args);
+  }
+
+}
---
+++ 06.bean-conditional/src/test/java/study/spring/beanconditional/BeanConditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.beanconditional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanConditionalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
