db0aeb3910506324a24ecce2211b192b81745fd8
YoungChulShin <go1323@gmail.com>
2023-08-19 06:07:13 +0900

    08-circuit-breaker.my: 프로젝트 추가

---
+++ 08.fault-tolerance/01.circuit-breaker/my-serivce/src/main/java/study/spring/circuitbreaker/my/MyApplication.java
@@ -0,0 +1,12 @@
+package study.spring.circuitbreaker.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyApplication.class, args);
+  }
+}
