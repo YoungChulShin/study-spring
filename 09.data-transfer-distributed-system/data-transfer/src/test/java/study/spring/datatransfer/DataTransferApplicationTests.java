97c0b794843b75c0a711730efe6a93b3db252a09
YoungChulShin <go1323@gmail.com>
2023-08-30 22:24:34 +0900

    09.distributed-system: 프로젝트 초안

---
+++ 09.data-transfer-distributed-system/data-transfer/src/test/java/study/spring/datatransfer/DataTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.datatransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTransferApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
