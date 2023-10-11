4fc88fe383b792a504280c9c6d5d1827bd62f15e
YoungChulShin <go1323@gmail.com>
2023-10-11 22:52:40 +0900

    09.schedule-job: scheduler 설정 추가

---
+++ 10.schedule-job/single-thread/src/main/java/study/spring/singlethread/config/SchedulerConfig.java
@@ -0,0 +1,10 @@
+package study.spring.singlethread;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+class SchedulerConfig {
+
+}
