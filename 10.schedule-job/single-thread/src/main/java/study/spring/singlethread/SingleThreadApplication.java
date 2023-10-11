94c87aed2c1065f66b0e149f84af93a2272e33a5
YoungChulShin <go1323@gmail.com>
2023-10-11 22:48:38 +0900

    09.schedule-job: single-thread project 추가

---
+++ 10.schedule-job/single-thread/src/main/java/study/spring/singlethread/SingleThreadApplication.java
@@ -0,0 +1,13 @@
+package study.spring.singlethread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleThreadApplication.class, args);
+	}
+
+}
---
+++ 10.schedule-job/single-thread/src/test/java/study/spring/singlethread/SingleThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.singlethread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
