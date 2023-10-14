7049481d80d211beb5c9bc7cd6a5d890083c94c1
YoungChulShin <go1323@gmail.com>
2023-10-14 21:34:03 +0900

    09.schedule-job: multi-thread 프로젝트 추가

---
+++ 10.schedule-job/multi-thread/src/test/java/study/spring/multithread/MultiThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.multithread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
