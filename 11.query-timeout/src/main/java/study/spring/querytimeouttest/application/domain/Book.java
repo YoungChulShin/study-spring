aa19e4fef702767b0791b180b1d99254e575acaf
YoungChulShin <go1323@gmail.com>
2023-11-01 22:59:33 +0900

    11.query-timeout: test entity(=book) 추가

---
+++ 11.query-timeout/src/main/java/study/spring/querytimeouttest/application/domain/Book.java
@@ -0,0 +1,23 @@
+package study.spring.querytimeouttest.application.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "author")
+  private String author;
+}
