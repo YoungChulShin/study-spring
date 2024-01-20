90ea59696b661959dec504cf0187eef700f7fe3a
YoungChulShin <go1323@gmail.com>
2024-01-20 22:55:40 +0900

    13.docurment: queryParameter에 swagger 설정 추가

--- 13.document-swagger/src/main/java/study/spring/documentswagger/presentation/OrganizationController.java
+++ 13.document-swagger/src/main/java/study/spring/documentswagger/presentation/OrganizationController.java
@@ -1,6 +1,8 @@
 package study.spring.documentswagger.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -9,6 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import study.spring.documentswagger.presentation.model.OrganizationDto;
 
@@ -29,8 +32,17 @@ public class OrganizationController {
           }
       )
   })
+  @Parameters(value = {
+      @Parameter(
+          name = "organizationCode",
+          description = "기관 코드",
+          required = false
+      )
+  })
   @GetMapping("/api/organizations")
-  public List<OrganizationDto> findOrganizations() {
+  public List<OrganizationDto> findOrganizations(
+      @RequestParam(name = "organizationCode", required = false) String organizationCode
+  ) {
     return Arrays.asList(new OrganizationDto("A", "서울시"));
   }
 
