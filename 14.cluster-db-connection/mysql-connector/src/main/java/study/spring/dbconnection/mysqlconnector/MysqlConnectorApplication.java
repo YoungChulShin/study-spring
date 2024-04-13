9338f5fb4d1231815f61e8179fad95afb2a278c3
YoungChulShin <go1323@gmail.com>
2024-04-13 23:42:51 +0900

    프로젝트 초안

---
+++ 14.cluster-db-connection/mysql-connector/src/main/java/study/spring/dbconnection/mysqlconnector/MysqlConnectorApplication.java
@@ -0,0 +1,13 @@
+package study.spring.dbconnection.mysqlconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlConnectorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MysqlConnectorApplication.class, args);
+  }
+
+}
---
+++ 14.cluster-db-connection/mysql-connector/src/test/java/study/spring/dbconnection/mysqlconnector/MysqlConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.dbconnection.mysqlconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlConnectorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
