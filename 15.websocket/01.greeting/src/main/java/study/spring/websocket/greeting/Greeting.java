ca5f7941207b59a6ea9ac012c489d15e474ee9fb
YoungChulShin <go1323@gmail.com>
2024-08-03 00:22:24 +0900

    모델 추가

---
+++ 15.websocket/01.greeting/src/main/java/study/spring/websocket/greeting/Greeting.java
@@ -0,0 +1,21 @@
+package study.spring.websocket.greeting;
+
+public class Greeting {
+
+  private String content;
+
+  public Greeting() {
+  }
+
+  public Greeting(String content) {
+    this.content = content;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+}
