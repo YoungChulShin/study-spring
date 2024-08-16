fb5a0423cceb295b433de5dfcac4e3e40aa7516f
YoungChulShin <go1323@gmail.com>
2024-08-17 01:04:13 +0900

    greeting-with-kafka: kafka로 받은 메시지를 다시 토픽에 전달

--- 15.websocket/02.greeting-with-kafka/src/main/java/study/spring/websocket/greetingwithkafka/kafka/KafkaMessageHandler.java
+++ 15.websocket/02.greeting-with-kafka/src/main/java/study/spring/websocket/greetingwithkafka/kafka/KafkaMessageHandler.java
@@ -2,17 +2,25 @@ package study.spring.websocket.greetingwithkafka.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+import study.spring.websocket.greetingwithkafka.Greeting;
 
 @Component
 public class KafkaMessageHandler {
+
+  private final SimpMessagingTemplate messagingTemplate;
+
+  public KafkaMessageHandler(SimpMessagingTemplate messagingTemplate) {
+    this.messagingTemplate = messagingTemplate;
+  }
 
   @KafkaListener(
       topics = KafkaConstant.MESSAGE_TOPIC,
       groupId = "myGroup1"
   )
-  public void handleMessage(ConsumerRecord<String, Object> record) {
-    String a = "aa";
+  public void handleMessage(ConsumerRecord<String, Greeting> record) {
+    messagingTemplate.convertAndSend("/topic/greetings", record.value());
 
   }
 }
